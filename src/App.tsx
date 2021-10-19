a4a24af1b09592d2a5f0e705b969f083fef5e5cc
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-19 16:08:03 +0900

    add colorScheme: teal for Button

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Button, ChakraProvider } from '@chakra-ui/react';
 function App() {
   return (
     <ChakraProvider>
-      <Button>ボタン</Button>
+      <Button colorScheme="teal">ボタン</Button>
     </ChakraProvider>
   );
 }
