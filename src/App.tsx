47c0abf8058e9aedc5c411e1639dc62254f36ff3
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-19 18:07:27 +0900

    add theme and set global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Button, ChakraProvider } from '@chakra-ui/react';
+import theme from './theme/theme';
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">ボタン</Button>
+      <p>ああああ</p>
     </ChakraProvider>
   );
 }
