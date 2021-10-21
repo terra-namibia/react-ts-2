a4d63ab1cb8c306ee61fe691051c91bd63dcf25a
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-21 17:47:10 +0900

    add header style by Flex and Heading of chakra ui

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,5 +1,19 @@
+import { Flex, Heading } from "@chakra-ui/layout";
 import { memo, VFC } from "react";
 
 export const Header: VFC = memo(() => {
-  return <div style={{height: "50px", backgroundColor: "teal"}} >Headerページです</div>;
+  return (
+    <Flex
+      as="nav"
+      bg="teal.500"
+      color="gray.50"
+      align="center"
+      justify="space-between"
+      padding={{ base: 3, md: 5 }}
+    >
+      <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+        ユーザー管理アプリ
+      </Heading>
+    </Flex>
+  );
 });
