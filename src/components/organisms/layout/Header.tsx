37ffec9d34287bf77cde2f18a0c0400a902f16f8
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-22 21:24:46 +0900

    add styles on Drawer

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -40,10 +40,10 @@ export const Header: VFC = memo(() => {
       <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen} >
         <DrawerOverlay>
           <DrawerContent>
-            <DrawerBody>
-              <Button>TOP</Button>
-              <Button>ユーザ一覧</Button>
-              <Button>設定</Button>
+            <DrawerBody p={0} bg="gray.100" >
+              <Button w="100%">TOP</Button>
+              <Button w="100%">ユーザ一覧</Button>
+              <Button w="100%">設定</Button>
             </DrawerBody>
           </DrawerContent>
         </DrawerOverlay>
