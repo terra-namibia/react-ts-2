1cb65f46eae04b823e6e071dce97a2523d1c7ad3
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-24 09:43:57 +0900

    modify Login page html

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,17 @@
+import { Button } from "@chakra-ui/button";
+import { Input } from "@chakra-ui/input";
+import { Box, Divider, Flex, Heading } from "@chakra-ui/layout";
 import { memo, VFC } from "react";
 
 export const Login: VFC = memo(() => {
-  return <p>Loginページです</p>;
+  return (
+    <Flex>
+      <Box>
+        <Heading as="h1">ユーザ管理アプリ</Heading>
+        <Divider />
+        <Input placeholder="ユーザID" />
+        <Button>ログイン</Button>
+      </Box>
+    </Flex>
+  );
 });
