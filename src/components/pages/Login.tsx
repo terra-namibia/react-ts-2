a64413ca55901fb6007515e5cdd548f7b9435b3f
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-24 10:59:21 +0900

    add css for Login component

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,16 +1,18 @@
 import { Button } from "@chakra-ui/button";
 import { Input } from "@chakra-ui/input";
-import { Box, Divider, Flex, Heading } from "@chakra-ui/layout";
+import { Box, Divider, Flex, Heading, Stack } from "@chakra-ui/layout";
 import { memo, VFC } from "react";
 
 export const Login: VFC = memo(() => {
   return (
-    <Flex>
-      <Box>
-        <Heading as="h1">ユーザ管理アプリ</Heading>
-        <Divider />
-        <Input placeholder="ユーザID" />
-        <Button>ログイン</Button>
+    <Flex alignItems="center" justifyContent="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">ユーザ管理アプリ</Heading>
+        <Divider my={4} />
+        <Stack spacing={10} py={4} px={10}>
+          <Input placeholder="ユーザID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>ログイン</Button>
+        </Stack>
       </Box>
     </Flex>
   );
