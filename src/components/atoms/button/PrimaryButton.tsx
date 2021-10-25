360ab800ba13a6690c935d95dd0da73dec635dfe
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-25 09:19:55 +0900

    ログイン機能実装

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -2,12 +2,13 @@ import { Button } from "@chakra-ui/button"
 import { memo, ReactNode, VFC } from "react";
 type Props = {
   children: ReactNode;
+  onClick: () => void;
 }
 
 export const PrimaryButton: VFC<Props> = memo((props) => {
-  const { children } = props;
+  const { children, onClick } = props;
   return (
-    <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+    <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }} onClick={onClick} >
       {children}
     </Button>
   );
--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,17 +1,23 @@
 import { Input } from "@chakra-ui/input";
 import { Box, Divider, Flex, Heading, Stack } from "@chakra-ui/layout";
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, useState, VFC } from "react";
+import { useAuth } from "../../hooks/useAuth";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
+  const [userId, setUserId] = useState('');
+  const {login, loading} = useAuth();
+  const onClickLogin = () => login(userId)
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => setUserId(e.target.value);
+
   return (
     <Flex alignItems="center" justifyContent="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
         <Heading as="h1" size="lg" textAlign="center">ユーザ管理アプリ</Heading>
         <Divider my={4} />
         <Stack spacing={10} py={4} px={10}>
-          <Input placeholder="ユーザID" />
-          <PrimaryButton>ログイン</PrimaryButton>
+          <Input placeholder="ユーザID" value={userId} onChange={onChangeUserId} />
+          <PrimaryButton onClick={onClickLogin} >ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
