8e58ecbbe39e591a5b3812df120e7f11e99c3600
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-25 21:01:06 +0900

    add Box and Stack for UserManagement.tsx

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,12 +1,33 @@
-import { Wrap, WrapItem } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/image";
+import { Box, Stack, Text, Wrap, WrapItem } from "@chakra-ui/layout";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <Wrap p={2} >
+    <Wrap p={{ base: 4, md: 10 }}>
       {[...Array(10)].map(() => (
         <WrapItem>
-          <div style={{width: '100px', height: '100px', backgroundColor: 'teal'}}>test</div>
+          <Box
+            w="260px"
+            h="260px"
+            bg="white"
+            borderRadius="10px"
+            shadow="md"
+            p={4}
+            _hover={{ cursor: "pointer", opacity: 0.8 }}
+          >
+            <Stack textAlign="center">
+              <Image
+                borderRadius="full"
+                boxSize="160px"
+                src="https://source.unsplash.com/random"
+                alt="プロフィール画像"
+                m="auto"
+              />
+              <Text fontSize="lg" fontWeight="bold">apple</Text>
+              <Text fontSize="sm" color="gray">apple</Text>
+            </Stack>
+          </Box>
         </WrapItem>
       ))}
     </Wrap>
