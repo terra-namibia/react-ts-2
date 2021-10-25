5ab07a6d24440aa89ef886850ca3600c9d39d79e
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-26 08:09:04 +0900

    separate UserCard component

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,33 +1,17 @@
-import { Image } from "@chakra-ui/image";
-import { Box, Stack, Text, Wrap, WrapItem } from "@chakra-ui/layout";
+import { Wrap, WrapItem } from "@chakra-ui/layout";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
     <Wrap p={{ base: 4, md: 10 }}>
       {[...Array(10)].map(() => (
         <WrapItem>
-          <Box
-            w="260px"
-            h="260px"
-            bg="white"
-            borderRadius="10px"
-            shadow="md"
-            p={4}
-            _hover={{ cursor: "pointer", opacity: 0.8 }}
-          >
-            <Stack textAlign="center">
-              <Image
-                borderRadius="full"
-                boxSize="160px"
-                src="https://source.unsplash.com/random"
-                alt="プロフィール画像"
-                m="auto"
-              />
-              <Text fontSize="lg" fontWeight="bold">apple</Text>
-              <Text fontSize="sm" color="gray">apple</Text>
-            </Stack>
-          </Box>
+          <UserCard
+            imageUrl="https://images.metmuseum.org/CRDImages/ep/original/LC-EP_1993_132_suppl_CH-003.jpg"
+            userName="Mike"
+            fullName="Mike News"
+          />
         </WrapItem>
       ))}
     </Wrap>
