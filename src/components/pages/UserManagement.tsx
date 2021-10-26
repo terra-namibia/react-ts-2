f2d0059c781ffaebc461def80afb276755918820
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-26 10:29:21 +0900

    get users from api by using hooks

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,19 +1,32 @@
-import { Wrap, WrapItem } from "@chakra-ui/layout";
-import { memo, VFC } from "react";
+import { Center, Wrap, WrapItem } from "@chakra-ui/layout";
+import { Spinner } from "@chakra-ui/spinner";
+import { memo, useEffect, VFC } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+  useEffect(() => getUsers(), [getUsers]);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      {[...Array(10)].map(() => (
-        <WrapItem>
-          <UserCard
-            imageUrl="https://images.metmuseum.org/CRDImages/ep/original/LC-EP_1993_132_suppl_CH-003.jpg"
-            userName="Mike"
-            fullName="Mike News"
-          />
-        </WrapItem>
-      ))}
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner color="teal.200" />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} >
+              <UserCard
+                imageUrl="https://images.metmuseum.org/CRDImages/ep/original/LC-EP_1993_132_suppl_CH-003.jpg"
+                userName="Mike"
+                fullName="Mike News"
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
